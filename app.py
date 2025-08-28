705e25a1f65e7dd900d1c489b1b59988d7ea0034
qazimohib <i212532@nu.edu.pk>
2025-08-28 17:55:56 +0500

    Initial Flask app setup with CI pipeline

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Welcome to Flask App</h1><a href="/about">About</a>'
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    return '<h1>Welcome to Flask App</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
