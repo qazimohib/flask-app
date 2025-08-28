641f506b61e206cf0bf9a632093aa9a3e2f259c1
qazimohib <i212532@nu.edu.pk>
2025-08-28 18:20:23 +0500

    Introduce style violations to test flake8

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
-from nonexistent_module import fake_function  # This will break CI
 
-app = Flask(__name__)
+app=Flask(__name__)    # Missing spaces around =
+
 
 @app.route('/')
 def home():
     return '<h1>Welcome to Flask App</h1><a href="/about">About</a>'
 
-@app.route('/about')
-def about():
+
+@app.route('/about') 
+def about( ):    # Extra spaces in function definition
     return render_template('about.html')
 
-if __name__ == '__main__':
+
+if __name__=='__main__':    # Missing spaces around ==
+    very_long_variable_name_that_exceeds_line_limit = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15
     app.run(debug=True)
